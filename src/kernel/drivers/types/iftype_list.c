b905e990695345309c5b67c0e4367df066ef141d
TimeStop34 <timestop34yt@gmail.com>
2025-11-22 14:55:26 +0300

    Обновил .git* файлы

--- src/kernel/drivers/types/iftype_list.c
+++ src/kernel/drivers/types/iftype_list.c
@@ -1,63 +1,11 @@
-#include ""
-enum drviflist {
-    Legacy, // 0x0
-    StorageMassController, // 0x01
-    NetworkController, // 0x02
-    Framebuffer, // 0x03
+#include "iftype_list.h"
 
-    /* 0x04, Multimedia Devices*/
-    MultimediaVideo,
-    MultimediaAudio,
-    MultimediaComputerTelephony,
-    MultimediaAudioDevice,
-    MultimediaOther,
+#define driver_interface(type, interface_manager) .type = interface_manager,
 
-    MemoryController, // 0x05
+#define DRVIFMANLIST \
+    driver_interface(Legacy, {}) \
+    
 
-    /* 0x06, Bridges */
-    BridgeHost,
-    BridgePCI,
-    BridgeLegacy,
-    BridgeOther,
-
-    /* 0x07, Simple Communication Controller */
-    SCCSerial,
-    SCCParallel,
-    SCCMultiportSerial,
-    SCCModem,
-    SCCOther,
-
-    /* 0x08, Base System Peripheral */
-    BSPPIC,
-    BSPDMA,
-    BSPTimer,
-    BSPRTC,
-    BSPPCIHotPlug,
-    BSPOther,
-
-    InputKeyboard,
-    InputPen,
-    InputMouse,
-    InputScanner,
-    InputGameport,
-    InputOther,
-
-    DockingStation, // 0x0A
-    Processor, // 0x0B
-
-    /* 0x0C, Serial Bus Controller */
+driver_interface_managers_list_t global_driver_interface_managers_list = {
+    DRVIFMANLIST
 };
-
-typedef struct
-{
-    int (*is_it_my_device) ();
-} driver_t;
-
-
-typedef struct {
-    void (*register_driver_type)(driver_t );
-} interface_manager_t;
-
-typedef struct {
-    interface_manager_t Legacy;
-} driver_interface_managers_list_t;
